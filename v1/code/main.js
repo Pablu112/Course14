947cb98c39e512acf5c47a38d1de54ba4f406c92
Pablu112 <yoohan438@gmail.com>
2025-12-11 19:54:13 +0500

    finish game main.js

--- v1/code/main.js
+++ v1/code/main.js
@@ -1,1 +1,24 @@
 kaplay();
+loadBean();
+setGravity(1600);
+
+const player = add([
+	sprite("bean"),
+	pos(center()),
+	area(),
+	body(),
+]);
+
+add([
+	rect(width(), 48),
+	outline(4),
+	area(),
+	pos(0, height() - 48),
+	body({ isStatic: true }),
+]);
+
+onClick(() => {
+	if (player.isGrounded()) {
+		player.jump();
+	}
+});
